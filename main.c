d57347ce7a9d1de7048a2342b5a98c836542560b
p156 <38420559+p156@users.noreply.github.com>
2018-11-06 03:12:49 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,45 @@
 
 int main(int argc, char *argv[]) 
 {
-    char str[7];
+    char str[100];
+    int array[100];
+    int i = 0;
+    int j;
     
     while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+        int n = atoi(str);
+        if(n > 0 && i == 0 && n < 100)
+        {
+           		array[0] = n;
+           		i++;
+       	}
+        else if(n == 0)
+    	{
+           		for(j = 0; j < i; j++)
+           		{
+               		printf("%d",array[j]);
+               		if(j < i - 1)
+               		{
+                   			printf(",");
+               		}
+           		}
+           		printf("\n");
+       	}
+        else if(n == -1)
+        {
+                printf("%d",array[0]);
+            	for(j = 0; j < i - 1; j++)
+            		{
+                		array[j] = array[j + 1];
+			        }
+			    i--;
+			    printf("\n");   
+        }
     }
-
     return 0;
 }
+/*
+0: 現在のスタックに格納されている全てのデータをスタックの上から順に「,」で区切って表示する
+-1: スタックからデータを取り出す
+それ以外の値: スタックにデータを追加する
+*/
